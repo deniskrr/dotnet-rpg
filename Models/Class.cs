72406e3fe35be8a4d9be6ee1461ccf0343eadb62
Denis Crăciunescu <dennis.craciunescu@gmail.com>
2022-01-31 18:40:26 +0200

    Add POST Character route

--- Models/Class.cs
+++ Models/Class.cs
@@ -1,9 +1,9 @@
 namespace dotnet_rpg.Models
 {
-    public enum Class
-    {
-      Knight = 1,
-      Mage = 2,
-      Cleric = 3
-    }
+  public enum Class
+  {
+    Knight = 1,
+    Mage = 2,
+    Cleric = 3
+  }
 }
